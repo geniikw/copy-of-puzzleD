f103232232a14ab78020d9ec050a6fa956352e48
geniikw <geniikw@gmail.com>
2014-07-30 12:00:06 +0900

    Update 0730

--- 2. Script/Board.cs
+++ 2. Script/Board.cs
@@ -1,32 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+public class Board : UITable 
+{
 
-
-public class Board : UITable {
-
-   
-    protected override void Start()
-    {
-        Init();
-        Reposition();
-        enabled = false;
-
-        for (int i = 0; i < transform.childCount; ++i)
-        {
-            Transform child = transform.GetChild(i);
-            child.GetComponent<Element>().Coord = new Vector2(i%6,i/6);
-            child.name = "Element" + i;
-        }
-
-    }
 }
-
-
-
 #if UNITY_EDITOR
 [CustomEditor(typeof(Board))]
 public class BoardEditor : Editor
@@ -35,6 +15,7 @@ public class BoardEditor : Editor
     {
         DrawDefaultInspector();
 
+        //UITable에서 Reposition을 버튼으로 사용하게 만듬
         Board myScript = target as Board;
         if (GUILayout.Button("Reposition Button"))
         {
